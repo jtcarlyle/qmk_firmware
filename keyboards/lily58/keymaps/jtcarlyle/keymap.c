2a0682d03c353d4b7575784bf110f773e50fadfd
John Carlyle <johncarlyle@Richards-iMac.local>
2023-04-19 17:02:25 -0700

    Add stenotype layer

--- keyboards/lily58/keymaps/jtcarlyle/keymap.c
+++ keyboards/lily58/keymaps/jtcarlyle/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "keymap_steno.h"
 
 enum layer_number {
   _XAH = 0,
@@ -6,21 +7,26 @@ enum layer_number {
   _MMOD,
   _NAV,
   _MNAV,
+  _PLV,
   _FUN,
   _MEDIA,
   _ADJUST,
 };
 
 // dual purpose tap-hold keys
-#define ST_TAB SFT_T(KC_TAB)
+#define CT_TAB CTL_T(KC_TAB)
 #define LT_SPC LT(_NAV, KC_SPC)
 #define MLT_SPC LT(_MNAV, KC_SPC)
-#define CT_ESC CTL_T(KC_ESC)
+#define GT_ESC GUI_T(KC_ESC)
 #define LT_BSPC LT(_FUN, KC_BSPC)
-#define LT_ENT LT(_FUN, KC_ENT)
-#define CT_DEL CTL_T(KC_DEL)
+#define ST_ENT SFT_T(KC_ENT)
+#define GT_DEL GUI_T(KC_DEL)
 #define AT_HOME ALT_T(KC_HOME)
 #define AT_MINS ALT_T(KC_MINS)
+
+// steno stuff
+#define ST_BOLT QK_STENO_BOLT
+#define ST_GEM  QK_STENO_GEMINI
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -44,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_BSLS, KC_QUOT, KC_COMM, KC_DOT, KC_P,    KC_Y,                     KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
   AT_HOME, KC_A,    KC_O,    KC_E,   KC_U,    KC_I,                     KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    AT_MINS,
   KC_LGUI, KC_SCLN, KC_Q,    KC_J,   KC_K,    KC_X, KC_LBRC,  KC_RBRC,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RGUI,
-                            KC_MEH, CT_ESC, LT_SPC,  ST_TAB,  LT_ENT,  LT_BSPC, CT_DEL, KC_HYPR
+                            KC_MEH, GT_ESC, LT_SPC,  CT_TAB,  ST_ENT,  LT_BSPC, GT_DEL, KC_HYPR
 ),
 /* QWE - QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -131,9 +137,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_MNAV] = LAYOUT(
   _______, _______,   _______,   _______,    _______,    _______,                     _______,    _______,    _______, _______,    _______,    _______,
   _______, C(KC_ESC), C(KC_SPC), A(KC_BSPC), A(KC_DEL),  _______,                     C(KC_Z),    A(KC_LEFT), KC_UP,   A(KC_RGHT), RCS(KC_Z),  C(KC_EQL),
-  _______, KC_LCTL,   KC_LALT,   KC_LGUI,    KC_LSFT,    _______,                     KC_HOME,    KC_LEFT,    KC_DOWN, KC_RGHT,    KC_END,     C(KC_MINS),
+  _______, KC_LGUI,   KC_LALT,   KC_LCTL,    KC_LSFT,    _______,                     C(KC_LEFT), KC_LEFT,    KC_DOWN, KC_RGHT,    C(KC_RIGHT),C(KC_MINS),
   _______, _______,   C(KC_X),   C(KC_C),    C(KC_V),    _______, _______, _______,   C(KC_F),    A(KC_UP),   KC_PGUP, KC_PGDN,    A(KC_DOWN), _______,
                                  _______,    _______,    _______, _______, S(KC_ENT), C(KC_G),    RCS(KC_G),  _______
+),
+/* PLOVER - A layer for stenotype
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  =   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |  \   |   "  |   ,  |   .  |   P  |   Y  |                    |   F  |   G  |   C  |   R  |   L  |  /   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |  ESC |   A  |   O  |   E  |   U  |   I  |-------.    ,-------|   D  |   H  |   T  |   N  |   S  |  -   |
+ * |------+------+------+------+------+------|  Alt  |    |  GUI  |------+------+------+------+------+------|
+ * |  [   |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   B  |   M  |   W  |   V  |   Z  |  ]   |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |  Meh |  Tab | Space| /  Ctrl /       \ Shift\  | BKSPC | Enter| Hyper|
+ *                   |      |      | NAV  |/       /         \      \ | FUN   |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+
+ [_PLV] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,                     STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC,
+  STN_FN,  STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1,                    STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,
+  XXXXXXX, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, DF(_XAH), XXXXXXX, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
+                             XXXXXXX, STN_PWR, STN_A,   STN_O,    STN_E,   STN_U,   STN_RE1, STN_RE2
 ),
 /* FUN - function keys and symbols
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -153,8 +181,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______,   _______,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   DF(_QWE),                   _______, KC_CIRC, KC_AMPR, KC_DLR,  KC_PERC,   _______,
   _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   DF(_XAH),                   _______, KC_LCBR, KC_LPRN, KC_LBRC, KC_ASTR,   KC_CAPS,
-  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  MO(_NAV), _______, _______, _______, KC_RCBR, KC_RPRN, KC_RBRC, TG(_MMOD), _______,
-                       _______, S(KC_ESC), S(KC_SPC),  S(KC_TAB), _______, _______, _______, _______
+  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  DF(_PLV), _______, _______, _______, KC_RCBR, KC_RPRN, KC_RBRC, TG(_MMOD), _______,
+                       _______, C(KC_ESC), C(KC_SPC),  C(KC_TAB), _______, _______, _______, _______
 ),
 
 [_MEDIA] = LAYOUT(
@@ -164,7 +192,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MRWD, KC_BRID, KC_BRIU, KC_MFFD, _______,
                    _______, _______, _______,  _______, _______, _______, _______, _______
 ),
-
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
