e4be6748fb65c807b9ed8fdde4caaa6f29366030
John Carlyle <jcarlyle@startmail.com>
2024-04-02 17:04:18 +0900

    Uncomitted mint changes (caution!)

--- keyboards/lily58/keymaps/jtcarlyle/keymap.c
+++ keyboards/lily58/keymaps/jtcarlyle/keymap.c
@@ -56,6 +56,7 @@ enum custom_keycodes {
 // #define DV_SPC  LT(_DVORAK, KC_SPC)
 // #define DV_ENT  LT(_DVORAK, KC_ENT)
 #define NAV_TAB LT(_NAV, KC_TAB)
+#define NAV_ENT LT(_NAV, KC_ENT)
 #define SM_BSPC LT(_SYM, KC_BSPC)
 #define AT_ESC  LALT_T(KC_ESC)
 #define AT_MINS RALT_T(KC_MINS)
@@ -181,9 +182,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  */
     [_QWERTY] = LAYOUT_LR(
-      KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,
-      NAV_TAB, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,
-      KC_ESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,
+      QK_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,
+      KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,
+      KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,
       KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_BSLS,
                               KC_LGUI , KC_LALT  , KC_SPC , KC_LCTL,
 
@@ -191,7 +192,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC,
                KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
       KC_RBRC, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-      SM_BSPC, KC_ENT, KC_RALT, KC_EQL
+      SM_BSPC, NAV_ENT, KC_RALT, KC_EQL
     ),
 
 /* Japanese layout
